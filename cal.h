e9e7c0d5714b797a1da3b0c66dddb08a7f21a1b3
IkaWaAyuMu <pungpond4450@gmail.com>
2021-02-18 01:49:05 +0700

    Update to the 2c57826 commit of cal.h.

--- cal.h
+++ cal.h
@@ -4,20 +4,22 @@
 #include <vector>
 #include <sstream>
 #include <ctime>
-using namespace std; 
+#pragma once
 
-class cal {
+namespace ical {
+
+    class calendar {
     public:
-        string timezone, DTSTART, DTEND, freq, weekstop, untillD, byday, location, Ename;
+        std::string timezone, DTSTART, DTEND, freq, weekstop, untillD, byday, location, Ename;
         // this is for testing purpose only need REAL UUID generator.
-        string UUID = "53688870-6D46-11EB-8572-0800200C9A66";
+        std::string UUID = "53688870-6D46-11EB-8572-0800200C9A66";
 
         // function to creat iCal event.
-        void createEvent(ofstream &file, string s) {   
+        void createEvent(std::ofstream& file, std::string s) {
             parseInfo(s);
             file << "BEGIN:VEVENT" << '\n';
-            file << "DTSTART; TZID=" << timezone << ":" << DTSTART << '\n';
-            file << "DTEND; TZID=" << timezone << ":" << DTEND << '\n';
+            file << "DTSTART:" << DTSTART << '\n';
+            file << "DTEND:" << DTEND << '\n';
             file << "RRULE:FREQ=" << freq << ";WKST=" << weekstop << ";UNTIL=" << untillD << ";BYDAY=" << byday << '\n';
             file << "DTSTAMP:" << getTstamp() + "000000" << '\n';
             file << "UID:" << UUID << '\n';
@@ -26,9 +28,9 @@ class cal {
             file << "END:VEVENT" << '\n';
 
         }
-        
+
         // function to create iCal Header.
-        void calHeader(ofstream &file) {
+        void calHeader(std::ofstream& file) {
             file << "BEGIN:VCALENDAR" << '\n';
             file << "PRODID:-//Uics Project//Schedule//EN" << '\n';
             file << "VERSION:2.0" << '\n';
@@ -42,19 +44,19 @@ class cal {
             file << "TZNAME:+07" << '\n';
             file << "DTSTART:19700101T000000" << '\n';
             file << "END:STANDARD" << '\n';
-            file << "END:VTIMEZONE" << '\n'; 
+            file << "END:VTIMEZONE" << '\n';
         }
 
         // function to create iCal Footer.
-        void calFooter(ofstream &file) {
+        void calFooter(std::ofstream& file) {
             file << "END:VCALENDAR";
         }
 
     private:
 
         // function to parse info from string and assign it to object.
-        void parseInfo(string s) {
-            vector<string> sArr = split(s, ','); 
+        void parseInfo(std::string s) {
+            std::vector<std::string> sArr = split(s, ',');
             Ename = sArr[0];
             byday = checkbyday(sArr[1]);
             DTSTART = checkDT(sArr[2], 0);
@@ -63,39 +65,41 @@ class cal {
         }
 
         // function to parse byday to iCal format **need more work.
-        string checkbyday(string s) {
+        std::string checkbyday(std::string s) {
             if (s == "Mth") return "MO,TH";
             if (s == "TuF") return "FR,TU";
             if (s == "We") return "WE";
             if (s == "Sa") return "SA";
             if (s == "Su") return "SU";
-            return "";       
+            return "";
         }
 
         // function to get Timestamp (when the function was called) in iCal format.
-        string getTstamp() {
+        std::string getTstamp() {
             time_t t = time(0);
             tm* now = localtime(&t);
-            string Tstamp;
-            if ((now->tm_mon+1) < 10) Tstamp = to_string(now->tm_year+1900) + "0" + to_string(now->tm_mon+1) + to_string(now->tm_mday) + "T";
-            else Tstamp = to_string(now->tm_year+1900) + to_string(now->tm_mon+1) + to_string(now->tm_mday) + "T";
+            std::string Tstamp;
+            if ((now->tm_mon + 1) < 10) Tstamp = std::to_string(now->tm_year + 1900) + "0" + std::to_string(now->tm_mon + 1) + std::to_string(now->tm_mday) + "T";
+            else Tstamp = std::to_string(now->tm_year + 1900) + std::to_string(now->tm_mon + 1) + std::to_string(now->tm_mday) + "T";
             return Tstamp;
         }
 
         // function that check the inputs and return Date/Time in iCal format.
-        string checkDT(string s, int pos) {
-            vector<string> arr = split(s, '-');
+        std::string checkDT(std::string s, int pos) {
+            std::vector<std::string> arr = split(s, '-');
             return getTstamp() + arr[pos].erase(arr[pos].find(':'), 1) + "00";
         }
 
         // function to split string.
-        vector<string> split(string s, char delimeter) {
-            vector<string> sArr;
-            string token;
-            istringstream tokenStream(s);
-            while (getline(tokenStream, token, delimeter)) {
+        std::vector<std::string> split(std::string s, char delimeter) {
+            std::vector<std::string> sArr;
+            std::string token;
+            std::istringstream tokenStream(s);
+            while (std::getline(tokenStream, token, delimeter)) {
                 sArr.push_back(token);
             }
-            return sArr; 
+            return sArr;
         }
-};
+    };
+
+} // end namespace ical 
