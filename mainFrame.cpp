0972a3233a0c964eca804ae46839ca4ea397a413
IkaWaAyuMu <pungpond4450@gmail.com>
2021-02-18 22:57:48 +0700

    Add schedule adding. For testing purpose only.

--- mainFrame.cpp
+++ mainFrame.cpp
@@ -3,25 +3,27 @@
 
 #include <wx/wxprec.h>
 #include <wx/valnum.h>
+#include <wx/timectrl.h>
+#include <wx/datectrl.h>
+#include <fstream>
+#include <string>
 
 enum
 {
-	//Menu
 	ID_New = 2,
-
-	//Add
-	ID_AddEvent = 11
+	ID_AddEvent = 11,
 };
 
 wxBEGIN_EVENT_TABLE(mainFrame, wxFrame)
 	EVT_MENU(ID_New, mainFrame::OnNew)
 	EVT_MENU(wxID_ABOUT, mainFrame::OnAbout)
 	EVT_MENU(wxID_EXIT, mainFrame::OnExit)
-	EVT_BUTTON(ID_AddEvent, mainFrame::ExportSchedule)
+	EVT_BUTTON(ID_AddEvent, mainFrame::AddSchedule)
 wxEND_EVENT_TABLE()
 
 mainFrame::mainFrame() : wxFrame(NULL, wxID_ANY, "Uics", wxPoint(100, 100), wxSize(750, 500))
 {
+	SetTextStyle();
 	//File
 	FileMenu = new wxMenu;		//Menu - File
 	FileMenu->Append(ID_New,		//New
@@ -46,21 +48,71 @@ mainFrame::mainFrame() : wxFrame(NULL, wxID_ANY, "Uics", wxPoint(100, 100), wxSi
 	SetStatusText("Uics by SixtyNine.");
 }
 
+void mainFrame::SetTextStyle()
+{
+	Header1 = new wxFont(15, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	Header2 = new wxFont(7, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	TextCtrl1 = new wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	TextCtrl2 = new wxFont(8, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+}
+
 void mainFrame::OnNew(wxCommandEvent& evt)
 {
 	wxIntegerValidator<int> DigitValidator;
+	//Header
+	AddClassScheduleHeader = new wxStaticText(this, wxID_ANY,
+		"Add class schedule",
+		wxPoint(10, 10));
+	AddClassScheduleHeader->SetFont(*Header1);
 	//SubjectID
 	SubjectIDLabel = new wxStaticText(this, wxID_ANY,
 		"Subject ID",
-		wxPoint(10, 10));
+		wxPoint(20, 35));
+	SubjectIDLabel->SetFont(*Header2);
 	SubjectIDTextCtrl = new wxTextCtrl(this, wxID_ANY,
 		"",
-		wxPoint(70, 10), wxSize(60, 30), 0,
+		wxPoint(20, 50), wxSize(60, 25), 0,
 		wxTextValidator(wxFILTER_DIGITS));
+	SubjectIDTextCtrl->SetFont(*TextCtrl1);
+	//SubjectName
+	SubjectNameLabel = new wxStaticText(this, wxID_ANY,
+		"Subject name",
+		wxPoint(90, 35));
+	SubjectNameLabel->SetFont(*Header2);
+	SubjectNameTextCtrl = new wxTextCtrl(this, wxID_ANY,
+		"",
+		wxPoint(90, 50), wxSize(200, 25));
+	SubjectNameTextCtrl->SetFont(*TextCtrl2);
+	//StartTime
+	StartTimeLabel = new wxStaticText(this, wxID_ANY,
+		"Start time",
+		wxPoint(300, 35));
+	StartTimeLabel->SetFont(*Header2);
+	StartTimePicker = new wxTimePickerCtrl(this, wxID_ANY,
+		wxDateTime(0,0,0,0),
+		wxPoint(300, 50), wxSize(85, 25));
+	StartTimePicker->SetFont(*TextCtrl2);
+	//EndTime
+	EndTimeLabel = new wxStaticText(this, wxID_ANY,
+		"End time",
+		wxPoint(395, 35));
+	EndTimeLabel->SetFont(*Header2);
+	EndTimePicker = new wxTimePickerCtrl(this, wxID_ANY,
+		wxDateTime(0, 0, 0, 0),
+		wxPoint(395, 50), wxSize(85, 25));
+	EndTimePicker->SetFont(*TextCtrl2);
+	//Location
+	LocationLabel = new wxStaticText(this, wxID_ANY,
+		"Location",
+		wxPoint(490, 35));
+	LocationtextCtrl = new wxTextCtrl(this, wxID_ANY,
+		"",
+		wxPoint(490, 50), wxSize(150, 25));
 	//Add
 	AddButton = new wxButton(this, ID_AddEvent,
-		"Add",
-		wxPoint(140, 10), wxSize(50, 30));
+		"Export",
+		wxPoint(10, 90), wxSize(100, 30));
+	AddButton->SetFont(*Header1);
 	FileMenu->Enable(ID_New, false);
 }
 
@@ -77,7 +129,27 @@ void mainFrame::OnExit(wxCommandEvent& event)
 		wxYES_NO | wxNO_DEFAULT);
 	if (confirmation == wxYES) Close(true);
 }
-void mainFrame::ExportSchedule(wxCommandEvent& event)
+void mainFrame::AddSchedule(wxCommandEvent& event)
 {
+	//Initialize the file.
+	std::ofstream dest("calendar.ics", std::ios::app);
+	//Copy from cal.h no further change now.
+	ical::calendar Event;
+	Event.calHeader(dest);
+	Event.timezone = "Asia/Bangkok";
+	Event.weekstop = "SU";
+	Event.freq = "WEEKLY";
+	Event.untillD = "20210228T000000Z";
+	//Get schedule time.
+	int StartHr, StartMin, StartSec;
+	StartTimePicker->GetTime(&StartHr, &StartMin, &StartSec);
+	int EndHr, EndMin, EndSec;
+	EndTimePicker->GetTime(&EndHr, &EndMin, &EndSec);
+	Event.createEvent(dest, 
+		std::string((SubjectIDTextCtrl->GetLineText(0)).mb_str()) + " " +std::string((SubjectNameTextCtrl->GetLineText(0)).mb_str()) + "," +
+		"TuF" + "," + std::to_string(StartHr) + ":" + std::to_string(StartMin) + "-" + std::to_string(EndHr) + ":" + std::to_string(EndMin) + "," + 
+		std::string((LocationtextCtrl->GetLineText(0)).mb_str()));
+	//Copy from cal.h no further change now.
+	Event.calFooter(dest);
 	event.Skip();
 }
--- mainFrame.h
+++ mainFrame.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <wx/wxprec.h>
+#include <wx/timectrl.h>
 
 class mainFrame : public wxFrame
 {
@@ -7,6 +8,8 @@ public:
 	mainFrame();
 
 public:
+	//TextStyle
+	wxFont *Header1, *Header2, *TextCtrl1, *TextCtrl2;
 	//MenuBar
 	wxMenuBar* menuBar = NULL;
 
@@ -15,16 +18,24 @@ public:
 	wxMenu* HelpMenu = NULL;		//Help
 
 	//Widgets
+	wxStaticText* AddClassScheduleHeader = NULL;
 	wxStaticText* SubjectIDLabel = NULL;
 	wxTextCtrl* SubjectIDTextCtrl = NULL;
 	wxStaticText* SubjectNameLabel = NULL;
 	wxTextCtrl* SubjectNameTextCtrl = NULL;
+	wxStaticText* StartTimeLabel = NULL;
+	wxTimePickerCtrl* StartTimePicker = NULL;
+	wxStaticText* EndTimeLabel = NULL;
+	wxTimePickerCtrl* EndTimePicker = NULL;
+	wxStaticText* LocationLabel = NULL;
+	wxTextCtrl* LocationtextCtrl = NULL;
 	wxButton* AddButton = NULL;
 	
 	//Functions
+	void SetTextStyle();
 	void OnNew(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 	void OnExit(wxCommandEvent& event);
-	void ExportSchedule(wxCommandEvent& event);
+	void AddSchedule(wxCommandEvent& event);
 	wxDECLARE_EVENT_TABLE();
 };
