50f65a8ddaec416f4cdd3cd25406aaf6688153ad
IkaWaAyuMu <pungpond4450@gmail.com>
2021-02-22 03:51:01 +0700

    Remove OnNew(), Make all windows managed by wxListbook.

--- mainFrame.h
+++ mainFrame.h
@@ -1,6 +1,8 @@
 #pragma once
+#include "ClassSchedule.h"
+
 #include <wx/wxprec.h>
-#include <wx/timectrl.h>
+#include <wx/listbook.h>
 
 class mainFrame : public wxFrame
 {
@@ -8,8 +10,12 @@ public:
 	mainFrame();
 
 public:
-	//TextStyle
-	wxFont *Header1 = NULL, *Header2 = NULL, *TextCtrl1 = NULL, *TextCtrl2 = NULL;
+	//Toolbook
+	wxListbook* listBook = NULL;
+
+	//Panel
+	ClassSchedule* ClassSchedulePanel = NULL;
+
 	//MenuBar
 	wxMenuBar* menuBar = NULL;
 
@@ -17,27 +23,9 @@ public:
 	wxMenu* FileMenu = NULL;		//File
 	wxMenu* HelpMenu = NULL;		//Help
 
-	//Widgets
-	wxStaticText* AddClassScheduleHeader = NULL;
-	wxStaticText* SubjectIDLabel = NULL;
-	wxTextCtrl* SubjectIDTextCtrl = NULL;
-	wxStaticText* SubjectNameLabel = NULL;
-	wxTextCtrl* SubjectNameTextCtrl = NULL;
-	wxStaticText* LocationLabel = NULL;
-	wxTextCtrl* LocationtextCtrl = NULL;
-	wxStaticText* StartTimeLabel = NULL;
-	wxTimePickerCtrl* StartTimePicker = NULL;
-	wxStaticText* EndTimeLabel = NULL;
-	wxTimePickerCtrl* EndTimePicker = NULL;
-	wxStaticText* DayLabel = NULL;
-	wxCheckBox *SunCheckmark = NULL, *MonCheckmark = NULL, *TueCheckmark = NULL, *WedCheckmark = NULL, *ThuCheckmark = NULL, *FriCheckmark = NULL, *SatCheckmark = NULL;
-	wxButton* AddButton = NULL;
-	
 	//Functions
-	void SetTextStyle();
-	void OnNew(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 	void OnExit(wxCommandEvent& event);
-	void AddSchedule(wxCommandEvent& event);
+	//void AddSchedule(wxCommandEvent& event);
 	wxDECLARE_EVENT_TABLE();
 };
