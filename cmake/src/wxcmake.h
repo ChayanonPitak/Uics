30979ce82295adae95a3c946131b08ca02dbf488
TanatTangun <ttangun1@gmail.com>
2021-02-22 17:20:56 +0700

    add cmake folder

---
+++ cmake/src/wxcmake.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <wx/frame.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/checkbox.h>
+#include <wx/app.h>
+#include <wx/gdicmn.h>
+#include <wx/string.h>
+#include <wx/windowid.h>
+#include <wx/msgdlg.h> 
+#include <wx/valtext.h>
+#include <wx/button.h>
+#include <wx/menu.h>
+
+
