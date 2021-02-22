b5907a8723284cd60c12d60cb2bb2f249fa5f8f7
IkaWaAyuMu <pungpond4450@gmail.com>
2021-02-22 22:15:41 +0700

    Remove header, add button.  Items reposition.

--- ClassSchedule.cpp
+++ ClassSchedule.cpp
@@ -21,95 +21,85 @@ ClassSchedule::ClassSchedule(wxWindow* Parent) : wxPanel(Parent, wxID_ANY, wxPoi
 {
 	SetTextStyle();
 	wxIntegerValidator<int> DigitValidator;
-	//Header
-	AddClassScheduleHeader = new wxStaticText(this, wxID_ANY,
-		"Add class schedule",
-		wxPoint(10, 10));
-	AddClassScheduleHeader->SetFont(Header1);
 	//SubjectID
 	SubjectIDLabel = new wxStaticText(this, wxID_ANY,
 		"Subject ID",
-		wxPoint(20, 35));
+		wxPoint(20, 10));
 	SubjectIDLabel->SetFont(Header2);
 	SubjectIDTextCtrl = new wxTextCtrl(this, wxID_ANY,
 		"",
-		wxPoint(20, 50), wxSize(60, 25), 0,
+		wxPoint(20, 25), wxSize(60, 25), 0,
 		wxTextValidator(wxFILTER_DIGITS));
 	SubjectIDTextCtrl->SetFont(TextCtrl1);
 	//SubjectName
 	SubjectNameLabel = new wxStaticText(this, wxID_ANY,
 		"Subject name",
-		wxPoint(90, 35));
+		wxPoint(90, 10));
 	SubjectNameLabel->SetFont(Header2);
 	SubjectNameTextCtrl = new wxTextCtrl(this, wxID_ANY,
 		"",
-		wxPoint(90, 50), wxSize(200, 25));
+		wxPoint(90, 25), wxSize(200, 25));
 	SubjectNameTextCtrl->SetFont(TextCtrl2);
 	//Location
 	LocationLabel = new wxStaticText(this, wxID_ANY,
 		"Location",
-		wxPoint(300, 35));
+		wxPoint(300, 10));
 	LocationLabel->SetFont(Header2);
 	LocationtextCtrl = new wxTextCtrl(this, wxID_ANY,
 		"",
-		wxPoint(300, 50), wxSize(150, 25));
+		wxPoint(300, 25), wxSize(150, 25));
 	LocationtextCtrl->SetFont(TextCtrl2);
 	//StartTime
 	StartTimeLabel = new wxStaticText(this, wxID_ANY,
 		"Start time",
-		wxPoint(20, 75));
+		wxPoint(20, 50));
 	StartTimeLabel->SetFont(Header2);
 	StartTimePicker = new wxTimePickerCtrl(this, wxID_ANY,
 		wxDateTime(0, 0, 0, 0),
-		wxPoint(20, 90), wxSize(85, 25));
+		wxPoint(20, 65), wxSize(85, 25));
 	StartTimePicker->SetFont(TextCtrl2);
 	//EndTime
 	EndTimeLabel = new wxStaticText(this, wxID_ANY,
 		"End time",
-		wxPoint(110, 75));
+		wxPoint(110, 50));
 	EndTimeLabel->SetFont(Header2);
 	EndTimePicker = new wxTimePickerCtrl(this, wxID_ANY,
 		wxDateTime(0, 0, 0, 0),
-		wxPoint(110, 90), wxSize(85, 25));
+		wxPoint(110, 65), wxSize(85, 25));
 	EndTimePicker->SetFont(TextCtrl2);
 	//Day
 	DayLabel = new wxStaticText(this, wxID_ANY,
 		"Day",
-		wxPoint(200, 75));
+		wxPoint(200, 50));
 	DayLabel->SetFont(Header2);
 	MonCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Monday",
-		wxPoint(200, 90));
+		wxPoint(200, 65));
 	MonCheckmark->SetFont(TextCtrl2);
 	TueCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Tuesday",
-		wxPoint(200, 105));
+		wxPoint(200, 80));
 	TueCheckmark->SetFont(TextCtrl2);
 	WedCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Wednesday",
-		wxPoint(270, 90));
+		wxPoint(270, 65));
 	WedCheckmark->SetFont(TextCtrl2);
 	ThuCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Thursday",
-		wxPoint(270, 105));
+		wxPoint(270, 80));
 	ThuCheckmark->SetFont(TextCtrl2);
 	FriCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Friday",
-		wxPoint(360, 97));
+		wxPoint(360, 72));
 	FriCheckmark->SetFont(TextCtrl2);
 	SatCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Saturday",
-		wxPoint(420, 90));
+		wxPoint(420, 65));
 	SatCheckmark->SetFont(TextCtrl2);
 	SunCheckmark = new wxCheckBox(this, wxID_ANY,
 		"Sunday",
-		wxPoint(420, 105));
+		wxPoint(420, 80));
 	SunCheckmark->SetFont(TextCtrl2);
-	//Add
-	AddButton = new wxButton(this, ID_AddEvent,
-		"Export",
-		wxPoint(10, 120), wxSize(100, 30));
-	AddButton->SetFont(Header1);
 }
 
 void ClassSchedule::SetTextStyle()
@@ -136,17 +126,17 @@ void ClassSchedule::AddSchedule(wxCommandEvent& event)
 	StartTimePicker->GetTime(&StartHr, &StartMin, &StartSec);
 	int EndHr, EndMin, EndSec;
 	EndTimePicker->GetTime(&EndHr, &EndMin, &EndSec);
-	unsigned int dayBinary =
-		MonCheckmark->GetValue() +
-		TueCheckmark->GetValue() * 2 +
-		WedCheckmark->GetValue() * 4 +
-		ThuCheckmark->GetValue() * 8 +
-		FriCheckmark->GetValue() * 16 +
-		SatCheckmark->GetValue() * 32 +
-		SunCheckmark->GetValue() * 64;
+	unsigned int dayBinary =				//Use addition to create binary array. Rightmost digit is monday
+		MonCheckmark->GetValue() +			//Mon 0b0000001
+		TueCheckmark->GetValue() * 2 +		//Tue 0b0000010
+		WedCheckmark->GetValue() * 4 +		//Wed 0b0000100
+		ThuCheckmark->GetValue() * 8 +		//Thu 0b0001000
+		FriCheckmark->GetValue() * 16 +		//Fri 0b0010000
+		SatCheckmark->GetValue() * 32 +		//Sat 0b0100000
+		SunCheckmark->GetValue() * 64;		//Sun 0b1000000
 	Event.createEvent(dest,
 		std::string((SubjectIDTextCtrl->GetLineText(0)).mb_str()) + " " + std::string((SubjectNameTextCtrl->GetLineText(0)).mb_str()),	//Name
-		std::string((LocationtextCtrl->GetLineText(0)).mb_str()),	//location
+		std::string((LocationtextCtrl->GetLineText(0)).mb_str()),	//location+
 		dayBinary,	//Day
 		StartHr, StartMin, StartSec, EndHr, EndMin, EndSec);	//Time
 	//Copy from cal.h no further change now.
