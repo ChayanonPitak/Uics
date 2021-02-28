139c7a6476bff2d75b15dbdcee2c559c385df4d0
SignatureJed <SignatureJed>
2021-02-28 12:24:27 +0700

    ExamSchedule Complete and add Comment

--- ExamSchedule.cpp
+++ ExamSchedule.cpp
@@ -1,11 +1,145 @@
 #include "ExamSchedule.h"
-
+#include <wx/timectrl.h>
+#include <fstream>
+#include <string>
+#include <wx/valnum.h>
 #include <wx/wxprec.h>
+
+enum
+{
+	ID_AddEvent = 11
+};
+
+wxBEGIN_EVENT_TABLE(ExamSchedule, wxPanel)
+EVT_BUTTON(ID_AddEvent, ExamSchedule::AddSchedule)
+wxEND_EVENT_TABLE()
+
 
 ExamSchedule::ExamSchedule(wxWindow* Parent) : wxPanel(Parent, wxID_ANY, wxPoint(0, 0), wxSize(750, 50))
 {
 	SetStyle();
+	wxIntegerValidator<int>digitvalidator;
+	//SubjectID
+	SubjectIDLabel = new wxStaticText(this, wxID_ANY,
+		"Subject ID",
+		wxPoint(20, 10));
+	SubjectIDLabel->SetFont(Header2);
+	SubjectIDTextCtrl = new wxTextCtrl(this, wxID_ANY,
+		"",
+		wxPoint(20, 25), wxSize(60, 25), 0,
+		wxTextValidator(wxFILTER_DIGITS));
+	SubjectIDTextCtrl->SetFont(TextCtrl1);
+	//SubjectName
+	SubjectNameLabel = new wxStaticText(this, wxID_ANY,
+		"Subject name",
+		wxPoint(90, 10));
+	SubjectNameLabel->SetFont(Header2);
+	SubjectNameTextCtrl = new wxTextCtrl(this, wxID_ANY,
+		"",
+		wxPoint(90, 25), wxSize(200, 25));
+	SubjectNameTextCtrl->SetFont(TextCtrl2);
+	//Location
+	LocationLabel = new wxStaticText(this, wxID_ANY,
+		"Location",
+		wxPoint(300, 10));
+	LocationLabel->SetFont(Header2);
+	LocationtextCtrl = new wxTextCtrl(this, wxID_ANY,
+		"",
+		wxPoint(300, 25), wxSize(150, 25));
+	LocationtextCtrl->SetFont(TextCtrl2);
+	//Comment
+	Comment = new wxStaticText(this, wxID_ANY,
+		"Comment",
+		wxPoint(460, 10));
+	Comment->SetFont(Header2);
+	CommentNameTextCtrl = new wxTextCtrl(this, wxID_ANY,
+		"",
+		wxPoint(460, 25), wxSize(250, 25));
+	CommentNameTextCtrl->SetFont(TextCtrl2);
+
+
+	//StartTime
+	StartTimeLabel = new wxStaticText(this, wxID_ANY,
+		"Start time",
+		wxPoint(20, 50));
+	StartTimeLabel->SetFont(Header2);
+	StartTimePicker = new wxTimePickerCtrl(this, wxID_ANY,
+		wxDateTime(0, 0, 0, 0),
+		wxPoint(20, 65), wxSize(85, 25));
+	StartTimePicker->SetFont(TextCtrl2);
+	//EndTime
+	EndTimeLabel = new wxStaticText(this, wxID_ANY,
+		"End time",
+		wxPoint(110, 50));
+	EndTimeLabel->SetFont(Header2);
+	EndTimePicker = new wxTimePickerCtrl(this, wxID_ANY,
+		wxDateTime(0, 0, 0, 0),
+		wxPoint(110, 65), wxSize(85, 25));
+	EndTimePicker->SetFont(TextCtrl2);
+	//Day
+	DayLabel = new wxStaticText(this, wxID_ANY,
+		"Day",
+		wxPoint(200, 50));
+	DayLabel->SetFont(Header2);
+	MonCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Monday",
+		wxPoint(200, 65));
+	MonCheckmark->SetFont(TextCtrl2);
+	TueCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Tuesday",
+		wxPoint(200, 80));
+	TueCheckmark->SetFont(TextCtrl2);
+	WedCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Wednesday",
+		wxPoint(270, 65));
+	WedCheckmark->SetFont(TextCtrl2);
+	ThuCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Thursday",
+		wxPoint(270, 80));
+	ThuCheckmark->SetFont(TextCtrl2);
+	FriCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Friday",
+		wxPoint(360, 72));
+	FriCheckmark->SetFont(TextCtrl2);
+	SatCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Saturday",
+		wxPoint(420, 65));
+	SatCheckmark->SetFont(TextCtrl2);
+	SunCheckmark = new wxCheckBox(this, wxID_ANY,
+		"Sunday",
+		wxPoint(420, 80));
+	SunCheckmark->SetFont(TextCtrl2);
+
 }
+
+void ExamSchedule::updateEvent(ical::event event) {
+	int StartHr, StartMin, StartSec;
+	int EndHr, EndMin, EndSec;
+	unsigned int dayBinary =
+		MonCheckmark->GetValue() +
+		TueCheckmark->GetValue() * 2 +
+		WedCheckmark->GetValue() * 4 +
+		ThuCheckmark->GetValue() * 8 +
+		FriCheckmark->GetValue() * 16 +
+		SatCheckmark->GetValue() * 32 +
+		SunCheckmark->GetValue() * 64;
+
+	//Get schedule time.
+	StartTimePicker->GetTime(&StartHr, &StartMin, &StartSec);
+	EndTimePicker->GetTime(&EndHr, &EndMin, &EndSec);
+
+	event.untillD = "20210228T000000Z";
+
+	event.name = std::string((SubjectIDTextCtrl->GetLineText(0).mb_str()))
+		+ " " + std::string((SubjectNameTextCtrl->GetLineText(0)).mb_str());
+	event.location = std::string((LocationtextCtrl->GetLineText(0)).mb_str());
+
+	event.day = ical::checkbyday(dayBinary);
+
+	event.DTstart = ical::checkDT(StartHr, StartMin, StartSec);
+	event.DTend = ical::checkDT(EndHr, EndMin, EndSec);
+}
+	
 
 void ExamSchedule::SetStyle()
 {
@@ -13,4 +147,14 @@ void ExamSchedule::SetStyle()
 	Header2 = wxFont(7, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
 	TextCtrl1 = wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
 	TextCtrl2 = wxFont(8, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+}
+
+void ExamSchedule::AddSchedule(wxCommandEvent& event)
+{
+	// update event obj
+	ExamSchedule::updateEvent(this->EVENT);
+	// append copy of event obj to list
+	this->listSchedule.push_back(this->EVENT);
+
+	event.Skip();
 }
--- ExamSchedule.h
+++ ExamSchedule.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <wx/wxprec.h>
+#include "cal.h"
+#include <wx/timectrl.h>
 
 class ExamSchedule : public wxPanel
 {
 public:
 	ExamSchedule(wxWindow* parent);
+	std::vector<ical::event> listSchedule;
+	
+	ical::event EVENT;
+	void updateEvent(ical::event event);
 
 public:
 	//Style
@@ -14,6 +20,25 @@ public:
 
 	//Functions
 	void SetStyle();
+	wxStaticText* SubjectIDLabel = NULL;
+	wxTextCtrl* SubjectIDTextCtrl = NULL;
+	wxStaticText* SubjectNameLabel = NULL;
+	wxTextCtrl* SubjectNameTextCtrl = NULL;
+	wxStaticText* LocationLabel = NULL;
+	wxTextCtrl* LocationtextCtrl = NULL;
+	wxStaticText* Comment = NULL;
+	wxTextCtrl *CommentNameTextCtrl = NULL;
+	wxStaticText* StartTimeLabel = NULL;
+	wxTimePickerCtrl* StartTimePicker = NULL;
+	wxStaticText* EndTimeLabel = NULL;
+	wxTimePickerCtrl* EndTimePicker = NULL;
+	wxStaticText* DayLabel = NULL;
+	wxCheckBox* SunCheckmark = NULL, * MonCheckmark = NULL, * TueCheckmark = NULL, * WedCheckmark = NULL, * ThuCheckmark = NULL, * FriCheckmark = NULL, * SatCheckmark = NULL;
+	wxButton* AddButton = NULL;
 
 	//wxDECLARE_EVENT_TABLE();
+	void SetTextStyle();
+	void AddSchedule(wxCommandEvent& event);
+
+	wxDECLARE_EVENT_TABLE();
 };
