6e1913602701cb930a9065fde740f6837c8e2786
IkaWaAyuMu <pungpond4450@gmail.com>
2021-03-11 19:58:15 +0700

    Add application icon.

--- src/mainApp.cpp
+++ src/mainApp.cpp
@@ -7,7 +7,9 @@ wxIMPLEMENT_APP(mainApp);
 
 bool mainApp::OnInit()
 {
+	wxInitAllImageHandlers();
 	mainFrame* _mainFrame = new mainFrame();
+	_mainFrame->SetIcons(wxIconBundle("..\\resources\\icon.png", wxBITMAP_TYPE_PNG));
 	_mainFrame->Show(true);
 	return true;
 }
