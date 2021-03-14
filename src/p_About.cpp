8dbeaf30a64b2d8c360b84cb33c08fa60ca5ffdf
Thidsloth <76035488+Thidtanai@users.noreply.github.com>
2021-03-14 16:35:40 +0700

    Enhance space

--- src/p_About.cpp
+++ src/p_About.cpp
@@ -5,19 +5,19 @@
 p_About::p_About(wxWindow* parent) :wxFrame(parent, wxID_ANY, "About", wxPoint(150, 150), wxSize(500, 500))
 {
 	//----------------------------------------Font------------------------------------------------------
-	HeaderFont = wxFont(15, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
-	HeaderFont_01 = wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
-	HeaderFont_02 = wxFont(8, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
-	NormalFont_01 = wxFont(8, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-	NormalFont_02 = wxFont(5, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-	SpecialFont_Head = wxFont(15, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_LIGHT);
-	SpecialFont_Text = wxFont(7, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_LIGHT);
+	HeaderFont = wxFont(18, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	HeaderFont_01 = wxFont(13, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	HeaderFont_02 = wxFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+	NormalFont_01 = wxFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	NormalFont_02 = wxFont(8, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+	SpecialFont_Head = wxFont(18, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_LIGHT);
+	SpecialFont_Text = wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_LIGHT);
 	//----------------------------------------Font------------------------------------------------------
 	//BigHead------------------------------
 
 	BigHeaderText = new wxStaticText(this, wxID_ANY, "About Uics", wxPoint(0, 0), wxSize(85, 25));
 	BigHeaderText->SetFont(HeaderFont);
-	BigHeaderText = new wxStaticText(this, wxID_ANY, "2021", wxPoint(120, 0), wxSize(85, 25));
+	BigHeaderText = new wxStaticText(this, wxID_ANY, "2021", wxPoint(135, 0), wxSize(85, 25));
 	BigHeaderText->SetFont(SpecialFont_Head);
 	//Header01-----------------------------
 	const int plus1 = 120;
@@ -25,35 +25,35 @@ p_About::p_About(wxWindow* parent) :wxFrame(parent, wxID_ANY, "About", wxPoint(1
 	HeaderText->SetFont(HeaderFont_01);
 	//Detail01
 	HeadText = new wxStaticText(this, wxID_ANY, "TANAT TANGUN", wxPoint(30, 80 + plus1), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "630610737", wxPoint(180, 80 + plus1), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "630610737", wxPoint(163, 80 + plus1), wxSize(85, 25));
 	Text = new wxStaticText(this, wxID_ANY, "-Project manager -Coding", wxPoint(50, 105 + plus1), wxSize(85, 75));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 	Text->SetFont(NormalFont_01);
 
 	HeadText = new wxStaticText(this, wxID_ANY, "CHAYANON PITAK", wxPoint(30, 130 + plus1), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "630610724", wxPoint(180, 130 + plus1), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "630610724", wxPoint(178, 130 + plus1), wxSize(85, 25));
 	Text = new wxStaticText(this, wxID_ANY, "-Project manager -Coding", wxPoint(50, 155 + plus1), wxSize(85, 75));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 	Text->SetFont(NormalFont_01);
 
 	HeadText = new wxStaticText(this, wxID_ANY, "CHETSADA CHAINANTI", wxPoint(30, 180 + plus1), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "630610723", wxPoint(180, 180 + plus1), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "630610723", wxPoint(210, 180 + plus1), wxSize(85, 25));
 	Text = new wxStaticText(this, wxID_ANY, "-Coding", wxPoint(50, 205 + plus1), wxSize(85, 165));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 	Text->SetFont(NormalFont_01);
 
 	HeadText = new wxStaticText(this, wxID_ANY, "TANANUN CHOWDEE", wxPoint(30, 230 + plus1), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "630610735", wxPoint(180, 230 + plus1), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "630610735", wxPoint(204, 230 + plus1), wxSize(85, 25));
 	Text = new wxStaticText(this, wxID_ANY, "-Coding", wxPoint(50, 255 + plus1), wxSize(85, 250));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 	Text->SetFont(NormalFont_01);
 
 	HeadText = new wxStaticText(this, wxID_ANY, "THIDTANAI KAEWPHET", wxPoint(30, 280 + plus1), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "630610741", wxPoint(180, 280 + plus1), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "630610741", wxPoint(210, 280 + plus1), wxSize(85, 25));
 	Text = new wxStaticText(this, wxID_ANY, "-UI/UX", wxPoint(50, 305 + plus1), wxSize(85, 300));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
@@ -64,15 +64,15 @@ p_About::p_About(wxWindow* parent) :wxFrame(parent, wxID_ANY, "About", wxPoint(1
 	HeaderText->SetFont(HeaderFont_01);
 	//Detail02
 	HeadText = new wxStaticText(this, wxID_ANY, "Virsion:", wxPoint(30, 80 + plus2), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "0.221", wxPoint(180, 80 + plus2), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "0.221", wxPoint(90, 82 + plus2), wxSize(85, 25));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 	HeadText = new wxStaticText(this, wxID_ANY, "Language:", wxPoint(30, 105 + plus2), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "English", wxPoint(180, 105 + plus2), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "English", wxPoint(110, 107 + plus2), wxSize(85, 25));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 	HeadText = new wxStaticText(this, wxID_ANY, "Last upadate:", wxPoint(30, 130 + plus2), wxSize(85, 25));
-	SpecialText = new wxStaticText(this, wxID_ANY, "3/14/2021", wxPoint(180, 130 + plus2), wxSize(85, 25));
+	SpecialText = new wxStaticText(this, wxID_ANY, "3/14/2021", wxPoint(130, 132 + plus2), wxSize(85, 25));
 	HeadText->SetFont(HeaderFont_02);
 	SpecialText->SetFont(SpecialFont_Text);
 }
