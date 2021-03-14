8cae1d515ce41a2fce72d27ab8c898a6d1917b65
Thidsloth <76035488+Thidtanai@users.noreply.github.com>
2021-03-14 14:29:18 +0700

    -Add Header font and text -Add Normal font and text -Add Special font and text

--- include/p_About.h
+++ include/p_About.h
@@ -8,10 +8,19 @@ public:
 	p_About(wxWindow* parent);
 public :
 	//Text
+	wxStaticText* BigHeaderText = NULL;
 	wxStaticText* HeaderText = NULL;
-
+	wxStaticText* HeadText = NULL;
+	wxStaticText* Text = NULL;
+	wxStaticText* SpacialText = NULL;
 	//Style
 	wxFont HeaderFont;
+	wxFont HeaderFont_01;
+	wxFont HeaderFont_02;
+	wxFont NormalFont_01;
+	wxFont NormalFont_02;
+	wxFont SpecialFont_Head;
+	wxFont SpecialFont_Text;
 
 	wxGridSizer* HeaderHerderTextsize = nullptr;
 };
