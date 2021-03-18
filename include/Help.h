15d932f9d638e782f271db6e9720f1baf97eb606
IkaWaAyuMu <pungpond4450@gmail.com>
2021-03-18 22:46:50 +0700

    Complete help.

--- include/Help.h
+++ include/Help.h
@@ -12,8 +12,10 @@ public:
 	//Listbook
 	wxListbook* PagesListBook = NULL;
 
-	wxStaticBitmap* Page1 = NULL;
-	wxStaticBitmap* Page2 = NULL;
+	//Bitmap
+	wxStaticBitmap* FilesBitmap = NULL;
+	wxStaticBitmap* MenuBitmap = NULL;
+	wxStaticBitmap* ToolsBitmap = NULL;
 };
 
 class PeriodHelpPanel : public wxPanel
@@ -21,7 +23,44 @@ class PeriodHelpPanel : public wxPanel
 public:
 	PeriodHelpPanel(wxWindow* Parent);
 public:
-	wxStaticBitmap* Page1 = NULL;
+	//Listbook
+	wxListbook* PagesListBook = NULL;
+
+	//Bitmap
+	wxStaticBitmap* SemesterPeriodBitmap = NULL;
+	wxStaticBitmap* ExaminationPeriodBitmap = NULL;
+	wxStaticBitmap* HolidayBitmap = NULL;
+	wxStaticBitmap* HolidayListBitmap = NULL;
+};
+
+class ClassScheduleHelpPanel : public wxPanel
+{
+public:
+	ClassScheduleHelpPanel(wxWindow* Parent);
+public:
+	//Listbook
+	wxListbook* PagesListBook = NULL;
+
+	//Bitmap
+	wxStaticBitmap* SubjectMetadataBitmap = NULL;
+	wxStaticBitmap* TimeBitmap = NULL;
+	wxStaticBitmap* DayBitmap = NULL;
+	wxStaticBitmap* ListBitmap = NULL;
+};
+
+class ExamScheduleHelpPanel : public wxPanel
+{
+public:
+	ExamScheduleHelpPanel(wxWindow* Parent);
+public:
+	//Listbook
+	wxListbook* PagesListBook = NULL;
+
+	//Bitmap
+	wxStaticBitmap* PeriodBitmap = NULL;
+	wxStaticBitmap* SubjectMetadataBitmap = NULL;
+	wxStaticBitmap* TimeBitmap = NULL;
+	wxStaticBitmap* ListBitmap = NULL;
 };
 
 class HelpFrame : public wxFrame
@@ -33,6 +72,8 @@ public:
 	wxListbook* MainListBook = NULL;
 
 	//Panel
-	IntroHelpPanel* _IntroHelpPanel = NULL;		//Intro
-	PeriodHelpPanel* _PeriodHelpPanel = NULL;	//Period
+	IntroHelpPanel* _IntroHelpPanel = NULL;					//Intro
+	PeriodHelpPanel* _PeriodHelpPanel = NULL;				//Period
+	ClassScheduleHelpPanel* _ClassScheduleHelpPanel = NULL;	//ClassSchedule
+	ExamScheduleHelpPanel* _ExamScheduleHelpPanel = NULL;	//ExanSchedule
 };
--- src/Help.cpp
+++ src/Help.cpp
@@ -7,38 +7,109 @@
 HelpFrame::HelpFrame(wxWindow* parent) : wxFrame(parent, wxID_ANY, "Help", wxPoint(150, 150), wxSize(1378, 811), wxDEFAULT_FRAME_STYLE & ~wxRESIZE_BORDER & ~wxMAXIMIZE_BOX)
 {
 	//Panel
-	IntroHelpPanel* _IntroHelpPanel = new IntroHelpPanel(this);		//Intro
-	PeriodHelpPanel* _PeriodHelpPanel = new PeriodHelpPanel(this);	//Period
+	_IntroHelpPanel = new IntroHelpPanel(this);					//Intro
+	_PeriodHelpPanel = new PeriodHelpPanel(this);				//Period
+	_ClassScheduleHelpPanel = new ClassScheduleHelpPanel(this);	//ClassSchedule
+	_ExamScheduleHelpPanel = new ExamScheduleHelpPanel(this);	//ExamSchedule
 
 	//ListBook
 	MainListBook = new wxListbook(this, wxID_ANY, wxPoint(0, 0), wxSize(1378, 811), wxLB_LEFT);
 	MainListBook->GetListView()->SetColumnWidth(0, 150);
 	MainListBook->AddPage(_IntroHelpPanel, "Introduction");
 	MainListBook->AddPage(_PeriodHelpPanel, "Semester Period");
+	MainListBook->AddPage(_ClassScheduleHelpPanel, "Class Schedule");
+	MainListBook->AddPage(_ExamScheduleHelpPanel, "Exam Schedule");
 };
 
 IntroHelpPanel::IntroHelpPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY, wxPoint(150, 0), wxSize(1228, 811))
 {
 	wxInitAllImageHandlers();
 	//Images
-	Page1 = new wxStaticBitmap(this, wxID_ANY,
-		wxBitmap("..\\resources\\Help_Intro_1.png", wxBITMAP_TYPE_PNG),
+	FilesBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\File_Guide.jpg", wxBITMAP_TYPE_JPEG),
 		wxPoint(0, 0));
-	Page2 = new wxStaticBitmap(this, wxID_ANY,
-		wxBitmap("..\\resources\\Help_Intro_2.png", wxBITMAP_TYPE_PNG),
+	MenuBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Section_Guide.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	ToolsBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Tool_Guide.jpg", wxBITMAP_TYPE_JPEG),
 		wxPoint(0, 0));
 
 	//Listbook
 	PagesListBook = new wxListbook(this, wxID_ANY, wxPoint(0, 0), wxSize(1228, 811), wxLB_TOP);
-	PagesListBook->AddPage(Page1, "Selection");
-	PagesListBook->AddPage(Page2, "Exporting");
+	PagesListBook->AddPage(FilesBitmap, "File Management");
+	PagesListBook->AddPage(MenuBitmap, "Menu");
+	PagesListBook->AddPage(ToolsBitmap, "Scanning Image");
 }
 
 PeriodHelpPanel::PeriodHelpPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY, wxPoint(150, 0), wxSize(1228, 811))
 {
 	wxInitAllImageHandlers();
 	//Images
-	Page1 = new wxStaticBitmap(this, wxID_ANY,
-		wxBitmap("..\\resources\\Help_SemesterPeriod_1.png", wxBITMAP_TYPE_PNG),
+	SemesterPeriodBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Semester\\Semester01.jpg", wxBITMAP_TYPE_JPEG),
 		wxPoint(0, 0));
+	ExaminationPeriodBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Semester\\Semester02.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	HolidayBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Semester\\Semester03.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	HolidayListBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Semester\\Semester04.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+
+	PagesListBook = new wxListbook(this, wxID_ANY, wxPoint(0, 0), wxSize(1228, 811), wxLB_TOP);
+	PagesListBook->AddPage(SemesterPeriodBitmap, "Semester Period");
+	PagesListBook->AddPage(ExaminationPeriodBitmap, "Examination Period");
+	PagesListBook->AddPage(HolidayBitmap, "Holiday");
+	PagesListBook->AddPage(HolidayListBitmap, "Holiday list managing");
+}
+
+ClassScheduleHelpPanel::ClassScheduleHelpPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY, wxPoint(150, 0), wxSize(1228, 811))
+{
+	wxInitAllImageHandlers();
+	//Images
+	SubjectMetadataBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\class_schedule\\ClassSchedule01.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	TimeBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\class_schedule\\ClassSchedule02.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	DayBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\class_schedule\\ClassSchedule03.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	ListBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\class_schedule\\ClassSchedule04.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+
+	PagesListBook = new wxListbook(this, wxID_ANY, wxPoint(0, 0), wxSize(1228, 811), wxLB_TOP);
+	PagesListBook->AddPage(SubjectMetadataBitmap, "Metadata");
+	PagesListBook->AddPage(TimeBitmap, "Time period");
+	PagesListBook->AddPage(DayBitmap, "Week interval");
+	PagesListBook->AddPage(ListBitmap, "List managing");
+}
+
+ExamScheduleHelpPanel::ExamScheduleHelpPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY, wxPoint(150, 0), wxSize(1228, 811))
+{
+	wxInitAllImageHandlers();
+	//Images
+	PeriodBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Exam_schedule\\Exam01.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	SubjectMetadataBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Exam_schedule\\Exam02.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	TimeBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Exam_schedule\\Exam03.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+	ListBitmap = new wxStaticBitmap(this, wxID_ANY,
+		wxBitmap("..\\resources\\Help\\Exam_schedule\\Exam04.jpg", wxBITMAP_TYPE_JPEG),
+		wxPoint(0, 0));
+
+	PagesListBook = new wxListbook(this, wxID_ANY, wxPoint(0, 0), wxSize(1228, 811), wxLB_TOP);
+	PagesListBook->AddPage(PeriodBitmap, "Period");
+	PagesListBook->AddPage(SubjectMetadataBitmap, "Metadata");
+	PagesListBook->AddPage(TimeBitmap, "Date and time");
+	PagesListBook->AddPage(ListBitmap, "List managing");
 }
